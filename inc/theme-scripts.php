9099319e31f04d4c75c79b03ca28fa6cd347240c
Thomas Bartels <tbartels@gmail.com>
2017-10-05 13:02:54 +0000

    move styles to shoelace framework

--- inc/theme-scripts.php
+++ inc/theme-scripts.php
@@ -1,13 +1,21 @@
 <?php
 /**
  * Enqueue scripts and styles.
+ * @package spanner
  */
 function spanner_scripts() {
+	$style_uri = get_stylesheet_directory_uri();
+	$template_uri = get_template_directory_uri();
+
 	wp_enqueue_style( 'spanner-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'theme-styles', path_join( $style_uri, 'css/styles.css' ) );
 
-	wp_enqueue_script( 'spanner-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
-	wp_enqueue_script( 'spanner-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'spanner-navigation',
+			path_join( $template_uri, 'js/navigation.js' ),
+			array(), '20151215', true );
+	wp_enqueue_script( 'spanner-skip-link-focus-fix',
+			path_join( $template_uri, 'js/skip-link-focus-fix.js' ),
+			array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
