724c2111ea124c80c460cb3fb7cafb111269657c
Thomas Bartels <tbartels@gmail.com>
2017-11-04 23:30:41 +0000

    add custom JS, remove unused JS

--- inc/theme-scripts.php
+++ inc/theme-scripts.php
@@ -15,12 +15,15 @@ function spanner_scripts() {
 	wp_enqueue_style( 'spanner-style', get_stylesheet_uri() );
 	wp_enqueue_style( 'theme-styles', path_join( $style_uri, 'css/styles.css' ) );
 
-	wp_enqueue_script( 'spanner-navigation',
-			path_join( $template_uri, 'js/navigation.js' ),
-			array(), '20151215', true );
+	//wp_enqueue_script( 'spanner-navigation',
+	//		path_join( $template_uri, 'js/navigation.js' ),
+	//		array(), '20151215', true );
 	wp_enqueue_script( 'spanner-skip-link-focus-fix',
 			path_join( $template_uri, 'js/skip-link-focus-fix.js' ),
 			array(), '20151215', true );
+	wp_enqueue_script( 'spanner-custom',
+			path_join( $template_uri, 'js/spanner.custom.js' ),
+			array(), '20171019', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
---
+++ js/spanner.custom.js
@@ -0,0 +1,37 @@
+(function () {
+	"use strict";
+
+	var handleClickMenuToggle, handleScrollMenuToggle, hasMenuOpen,
+		bodyClass = document.body.classList;
+
+	hasMenuOpen = function () {
+		return bodyClass.contains('is-menu-active'); // open menu alredy active
+	}
+
+	handleClickMenuToggle = function (evt) {
+		var isMenuTog = evt.target.classList.contains('menu-toggle'), // click on toggle button
+			isOffMenu = evt.target.closest('.entry-content') !== null; // click outside of the menu
+
+		if (isMenuTog || (hasMenuOpen() && isOffMenu)) {
+			bodyClass.toggle('is-menu-active');
+			evt.target.blur();
+		}
+	};
+
+	// TODO
+	handleScrollMenuToggle = function (evt) {
+		if (hasMenuOpen()) {
+			if (evt.type === 'touchmove') {
+				console.log( evt );
+				//console.log( 'client', evt.changedTouches[0].clientX, evt.changedTouches[0].clientY );
+			} else if (evt.type === 'scroll') {
+				console.log( evt );
+			}
+			//bodyClass.toggle('is-menu-active');
+		}
+	};
+
+	document.body.addEventListener('click', handleClickMenuToggle);
+	//window.addEventListener('scroll', handleScrollMenuToggle);
+	//window.addEventListener('touchmove', handleScrollMenuToggle);
+})();
