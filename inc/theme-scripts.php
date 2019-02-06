5c6e0414f8cd73ee426216adfee4cf5da458237d
Thomas Bartels <tbartels@gmail.com>
2019-02-06 13:04:22 +0000

    remove duplicate styles enqueue

--- inc/theme-scripts.php
+++ inc/theme-scripts.php
@@ -4,7 +4,6 @@
  * @package spanner
  */
 function spanner_scripts() {
-	$style_uri = get_stylesheet_directory_uri();
 	$template_uri = get_template_directory_uri();
 
 	wp_enqueue_style( 'font-google-heebo', 'https://fonts.googleapis.com/css?family=Heebo:100,300,400,700' );
@@ -14,11 +13,10 @@ function spanner_scripts() {
 
 	wp_enqueue_style( 'spanner-styles', path_join( $template_uri, 'css/styles.css' ) );
 	wp_enqueue_style( 'theme-style', get_stylesheet_uri() );
-	wp_enqueue_style( 'theme-styles', path_join( $style_uri, 'css/styles.css' ) );
 
-	//wp_enqueue_script( 'spanner-navigation',
-	//		path_join( $template_uri, 'js/navigation.js' ),
-	//		array(), '20151215', true );
+	#wp_enqueue_script( 'spanner-navigation',
+	#		path_join( $template_uri, 'js/navigation.js' ),
+	#		array(), '20151215', true );
 	wp_enqueue_script( 'spanner-skip-link-focus-fix',
 			path_join( $template_uri, 'js/skip-link-focus-fix.js' ),
 			array(), '20151215', true );
