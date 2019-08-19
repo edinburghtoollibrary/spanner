022b39d43d00846ec3862942e2db02178e18b49b
Thomas Bartels <tbartels@gmail.com>
2019-08-19 10:59:36 +0000

    add target for label on search form

--- searchform.php
+++ searchform.php
@@ -1,7 +1,5 @@
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<label>
-		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ); ?></span>
-		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
-  </label>
+	<label for="s"><span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ); ?></span></label>
+	<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
   <input type="submit" class="lnr search-submit" value="<?php echo esc_attr_x( '&#xe86f;', 'submit button' ); ?>" />
 </form>
